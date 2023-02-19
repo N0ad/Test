b1b2de73cfa8f52c7bfdae9b7084959c02b574b6
GI <g.ivanov2@g.nsu.ru>
2023-02-19 08:38:39 +0000

    Task 1

---
+++ testF.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <chrono>
+using namespace std;
+
+float arr[10000000];
+
+int main() {
+	float sum = 0;
+#pragma acc enter data create(arr[0:10000000], sum)
+	float part = 2 * acos(-1) / 9999999;
+	auto start = chrono::high_resolution_clock::now();
+#pragma acc parallel loop present(arr[0:10000000])
+	for (int i = 0; i < 10000000; i++) {
+		arr[i] = sin(part * i);
+	}
+#pragma acc parallel loop present(arr[0:10000000], sum) reduction(+:sum)
+	for (int i = 0; i < 10000000; i++) {
+		sum = sum + arr[i];
+	}
+	auto elapsed = chrono::high_resolution_clock::now() - start;
+	long long msec = chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+#pragma acc exit data delete(arr[0:10000000]) copyout(sum)
+	cout << sum << "/n";
+	cout << msec;
+	return 0;
+}
